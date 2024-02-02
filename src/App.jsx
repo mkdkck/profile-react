115e94db7d12ce83181445beffb77c0355727e7d
mkdkck <colin173@gmail.com>
2024-02-03 00:10:41 +1030

    added basic file structures

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { Header } from './components/Header'
+import { Footer } from './components/Footer'
+import { Navigation } from './components/Navigation'
+
+
+
+function App() {
+  return (
+    <div className="">
+      <Header />
+      <Navigation />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,4 @@
+import { About } from './pages/About';
+import { Contact } from './pages/Contact';
+import { Portfolio } from './pages/Portfolio';
+import { Resume } from './pages/Resume'
